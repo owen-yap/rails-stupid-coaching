bd3d2162eb2849f818fa62ef7b71823da267dc66
Owen Yap <owen.y.yuhan@gmail.com>
2020-11-23 12:59:08 +0800

    Added answer page and some styles

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,14 @@
 class QuestionsController < ApplicationController
   def ask; end
+
+  def answer
+    @response = ''
+    @response = if params[:question] == 'I am going to work'
+                  'Great!'
+                elsif params[:question][-1] == '?'
+                  'Silly question, get dressed and go to work!'
+                else
+                  "I don't care, get dressed and go to work!"
+                end
+  end
 end
